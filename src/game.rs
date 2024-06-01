8e142eb4911a25423342eca9a4595c468f4cb028
Raishav Hanspal <rhanspal25@gmail.com>
2024-06-02 00:44:39 +0530

    update symbols, compile as wasm for web

---
+++ src/game.rs
@@ -0,0 +1,48 @@
+use bevy::prelude::*;
+#[derive(Component)]
+struct Movable;
+
+const SYM_HEIGHT: f32 = 122.;
+const SYM_COUNT: f32 = 5.;
+pub fn start() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, init)
+        .add_systems(Update, tick)
+        .run();
+}
+
+fn init(commands: Commands, asset_server: Res<AssetServer>) {
+    show_sprite(commands, asset_server)
+}
+
+fn show_sprite(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn(Camera2dBundle::default());
+    commands.spawn(SpriteBundle {
+        texture: asset_server.load("../assets/scorpion.png"),
+        ..default()
+    });
+    let mut i: f32 = 0.;
+    while i < SYM_COUNT {
+        i += 1.;
+        let sprite_path = format!("../assets/{}.png", i);
+        println!("{sprite_path}");
+        commands.spawn((
+            SpriteBundle {
+                transform: Transform::from_xyz(0., SYM_HEIGHT * (i - SYM_COUNT + 1.), 0.), //-500, 0, 500
+                texture: asset_server.load(sprite_path),
+                ..default()
+            },
+            Movable,
+        ));
+    }
+}
+
+fn tick(time: Res<Time>, mut sprite_position: Query<(&Movable, &mut Transform)>) {
+    for (_, mut transform) in &mut sprite_position {
+        transform.translation.y -= SYM_HEIGHT * time.delta_seconds();
+        if transform.translation.y < -1. * (SYM_COUNT / 2.) * SYM_HEIGHT {
+            transform.translation.y += SYM_HEIGHT * SYM_COUNT;
+        }
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,16 @@
 use wasm_bindgen::prelude::*;
+mod game;
 #[wasm_bindgen]
-extern {
-    fn alert(s:&str);
+extern "C" {
+    fn alert(s: &str);
 }
 
 #[wasm_bindgen]
 pub fn greet() {
     alert("yoyo");
+}
+
+#[wasm_bindgen]
+pub fn start_game() {
+    game::start();
 }
